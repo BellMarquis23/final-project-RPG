f9726060914a7632722bab08ebeea49474d77d04
Marcus Ponce <BellMarquis23@users.noreply.github.com>
2018-11-26 14:56:12 -0800

    first push

---
+++ settings.py
@@ -0,0 +1,27 @@
+#this file was created by Marcus Ponce
+#thanks Chris Bradfield with Kids Can Code basic format
+
+#define some colors (R,G,B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+
+#game settings
+WIDTH = 1024  #16* 64 or 32*32 or 64*16
+HEIGHT = 768 #16* 48 or 32*24 or 64*12
+FPS = 60
+TITLE = "Tilemap Project"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+#player settings
+PLAYER_SPEED = 300
+PLAYER_IMG = 
